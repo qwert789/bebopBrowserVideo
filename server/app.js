c842e265bd8693b88e87e66aacfc44cee4e68bfa
qwert789 <qwert789@gmail.com>
2016-03-05 11:27:05 +0200

    Enable video stream bebop 2

--- server/app.js
+++ server/app.js
@@ -5,7 +5,9 @@ var drone = bebop.createClient();
 
 drone.connect(function() {
   console.log('connected to drone')
-})
+});
+
+drone.MediaStreaming.videoEnable(1); // this line should start video
 
 io.on('connection', function (socket) {
   drone.on('video', function (data) {
